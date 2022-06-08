31f4f99cd21c7cffbaaf92df28cce70a0de2dc24
ZahraAbaszadeh <zahraabaszade96@gmail.com>
2022-06-08 17:48:27 +0430

    finish product Category Layout

---
+++ src/layouts/index.js
@@ -0,0 +1,5 @@
+export { Footer } from './footer/footerLayout';
+export { Header } from './header/headerLayout';
+export { Sidebar } from './sidebar/sidebarLayout';
+export { DashboardLayout } from './dashboard/dashboardLayout';
+export { ProductCategory } from './productCategory/productCategoryLayout';
