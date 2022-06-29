3b40128b2e5c68179c14446b23c023778884a766
ZahraAbaszadeh <zahraabaszade96@gmail.com>
2022-06-30 00:18:17 +0430

    not found page

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+export { BasketPage } from "./Basket/BasketPage";
+export { CheckoutPage } from "./Checkout/CheckoutPage";
+
+export {
+  UserOrdersPage,
+  ProtectedOrdersPage,
+  UserProductPage,
+  UserQuantityPage,
+  UserExitPage,
+  UserCategoryPage,
+} from "./Dahsboard";
+
+export { HomePage } from "./Home/HomePage";
+export { LoginPage } from "./Login/LoginPage";
+export { NotFoundPage } from "./NotFound/NotFoundPage";
+
+export { PaymentPage } from "./Payment/PaymentPage";
+
+export { ProductPage } from "./Product/ProductPage";
+export { ProductsPage } from "./Products/ProductsPage";
