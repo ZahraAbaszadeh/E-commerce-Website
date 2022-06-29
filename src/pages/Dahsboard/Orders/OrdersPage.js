81f656ae9495b15cfa19875860a127ba69c676e6
ZahraAbaszadeh <zahraabaszade96@gmail.com>
2022-06-30 00:29:24 +0430

    category

---
+++ src/pages/Dahsboard/Orders/OrdersPage.js
@@ -0,0 +1,23 @@
+import { DashboardLayout, Footer, Header } from 'layouts';
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+export const UserOrdersPage = (props) => {
+    return (
+        <div className="contentWithHeaderAndFooter">
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>سفارشات | {process.env.REACT_APP_WEBSITE_NAME}</title>
+                <meta name="description" content="سفارشات %REACT_APP_WEBSITE_NAME%" />
+            </Helmet>
+
+            <Header/>
+
+            <DashboardLayout>
+                سفارشات کاربری
+            </DashboardLayout>
+
+            <Footer/>
+        </div>
+    );
+};
